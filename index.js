3edfc0d61f3679ca7fdfdd9450c28d4d445f7fef
Sasu Osayande <osayande.sasu@outlook.com>
2022-02-18 15:30:53 -0500

    refactored to use OOP

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/**
 // Arrays to keep track of each task's state
 
 // Create a new task by adding to the arrays
@@ -36,3 +37,32 @@ const tasks = [task1, task2];
 task1.logTaskState(); // Clean Cat Litter has not been completed
 task1.completeTask();
 task1.logTaskState(); // Clean Cat Litter has been completed
+
+**/
+
+function newTask(title, description) {
+  const task = {
+    title,
+    description,
+    complete: false,
+    completeTask: function() {
+      this.complete = true;
+    },
+    logTaskState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+  };
+  return task;
+}
+
+// DRIVER CODE BELOW
+
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box"); // task 0
+const task2 = newTask("Do Laundry", "😨"); // task 1
+const tasks = [task1, task2];
+
+task1.logTaskState(); // Clean Cat Litter has not been completed
+task1.completeTask();
+task1.logTaskState(); // Clean Cat Litter has been completed
+
+console.log(tasks);
